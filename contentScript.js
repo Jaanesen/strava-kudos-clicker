afb4b954545511cce5238d21f837677ba7c7fa88
jonathan.aanesen <jonathan.aanesen@kredinor.no>
2024-05-15 13:53:48 +0200

    fix: can now be first to give kudos

--- contentScript.js
+++ contentScript.js
@@ -1,7 +1,8 @@
 function clickAllKudos() {
-  let kudosButtons = document.querySelectorAll(
-    'button[title="Give kudos"][data-testid="kudos_button"]'
+  var kudosButtons = document.querySelectorAll(
+    'button[title="Give kudos"][data-testid="kudos_button"],button[title="Be the first to give kudos!"][data-testid="kudos_button"]'
   )
+
   kudosButtons.forEach((button) => {
     console.log('Clicking kudos button, ', button)
     button.click()
